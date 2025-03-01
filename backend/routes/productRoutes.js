401fb82d34e6f820c15a9c23da41857a3a73053f
Hashir Khan <hashirahmedkhan123@gmail.com>
2025-03-01 17:35:29 +0500

    cart model done

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -17,7 +17,7 @@ router.get('/get-all-products', getAllProducts);
 router.get('/:id', getProductById);
 
 // Admin-only Routes
-router.post('/add-product', authenticateJWT, adminCheck,upload.array('images', 1), createProduct);
+router.post('/add-product', authenticateJWT, adminCheck,upload.array('images', 5), createProduct);
 router.put('/update-product/:id', authenticateJWT, adminCheck,upload.array('images', 5), updateProduct);
 router.delete('/delete-product/:id', authenticateJWT, adminCheck, deleteProduct);
 router.get('/search', searchProducts);
